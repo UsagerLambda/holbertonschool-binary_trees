8553030662acad9981e90331812eb80f58116a52
UsagerLambda <clement.chassem@gmail.com>
2024-08-05 11:49:49 +0200

    Fix P0T2 & Fix betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_right - entry
- * @parent: --
- * @value: --
- * Return: --
+ * binary_tree_insert_right - Inserts a node as the right-child of another node
+ * @parent: A pointer to the node to insert the right-child in
+ * @value: The value to store in the new node
+ * Return: A pointer to the created node, or NULL on failure or parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
@@ -18,10 +18,14 @@ return (NULL);
 
 new_node->n = value;
 new_node->left = NULL;
-/* Si le parent a un droit, il devient le droit du nouveau nœud */
+/* new_node->right prend l'adresse de parent->right */
 new_node->right = parent->right;
-/* Le nouveau nœud devient l'enfant droit du parent */
-parent->right = new_node;
+if (new_node->right != NULL) /* Vérif si parent avait un enfant droit */
+	/* update parent de new_node->right vers new_node */
+	new_node->right->parent = new_node;
+/* set new_node comme enfant droit de parent */
+parent->right = new_node; /* pos new_node comme enfant droit de parent */
+new_node->parent = parent; /* le parent de new_node deviens parent */
 
 return (new_node);
 }
