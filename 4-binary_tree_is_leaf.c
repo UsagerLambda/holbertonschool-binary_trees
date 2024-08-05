acdbdf5602ed4585b0704b0caa7192a68e4cffe9
UsagerLambda <clement.chassem@gmail.com>
2024-08-05 13:20:21 +0200

    fix betty P0T4, add file P0T6

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_leaf - e
- * @node: -
- * Return: -
+ * binary_tree_is_leaf - check if node is a leaf
+ * @node: node to check
+ * Return: 0 if node == NULL, 1 if the childs of node is NULL
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - NULL
+ * @tree: NULL
+ * @func: NULL
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+}
